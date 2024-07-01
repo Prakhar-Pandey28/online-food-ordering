65cf5ef8c572755d866b7b2660d5392262727fce
Prakhar <prakharpandey2811@gmail.com>
2024-07-02 01:26:26 +0530

    initialize

--- FoodOnline/test_time.py
+++
@@ -1,9 +0,0 @@
-from datetime import time,datetime,date
-
-today_date=date.today()
-today=today_date.isoweekday()
-print(today_date,today)
-now=datetime.now()
-print(now)
-current_time=now.strftime("%I:%M:%p")
-print(current_time)
